c4da9a923d0b4d95a1216d622922338a53e3166a
Abdul Alim <aalim@deqode.com>
2026-09-17 12:46:13 +0530

    add item search end point

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ def welcome():
 def get_single_item(item_id: int):
     """Get a single item by its ID."""
     return {"success": True, "message": f"Your item id is {item_id}"}
+
+@app.get("/search")
+def search_item(q: str | None = None):
+    """Search for items using the provided search query."""
+    return {"success" : True, "message": f"Your search term : {q}"}
