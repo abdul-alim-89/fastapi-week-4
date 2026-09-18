e8f146e5929756fb8d69e1ad4806ee7b818f5db6
Abdul Alim <aalim@deqode.com>
2026-09-18 17:02:10 +0530

    add think about question answer

--- routers/products.py
+++ routers/products.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Request
 from models.schemas import ProductCreate
 
 router = APIRouter(prefix="/products", tags=["Product"])
