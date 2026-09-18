50d231e059285c0e12498ed15d64a0bda743ce19
Abdul Alim <aalim@deqode.com>
2026-09-18 16:21:39 +0530

    add items router

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from fastapi import FastAPI
 from routers.products import router as product_router
+from routers.items import router as item_router
 
 app = FastAPI()
 
 app.include_router(product_router)
-
-@app.get("/")
-def welcome():
-    """Initial function"""
-    return {"message" : "Welcome to fastapi"}
-
-@app.get("/item/{item_id}")
-def get_single_item(item_id: int):
-    """Get a single item by its ID."""
-    return {"success": True, "message": f"Your item id is {item_id}"}
-
-@app.get("/search")
-def search_item(q: str | None = None):
-    """Search for items using the provided search query."""
-    return {"success" : True, "message": f"Your search term : {q}"}
+app.include_router(item_router)
 
